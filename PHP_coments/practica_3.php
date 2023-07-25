92bee9c15fb44ad1db0ae0a62fe2102788d628ce
aristides1000 <aristides1000@gmail.com>
2023-07-25 10:52:13 -0400

    agregadas prácticas php

---
+++ PHP_coments/practica_3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <?php
+    // tu puedes incluso comentar partes de codigo para que no se ejecuten
+
+    $x = 5 /* + 15 */ + 5;
+    echo $x;
+  ?>
+
+</body>
+</html>
