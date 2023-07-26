56d03f5906cd51b00f8246bc148fe23718de2d6c
aristides1000 <aristides1000@gmail.com>
2023-07-26 10:51:39 -0400

    agregada info

---
+++ PHP_Variables/practica_1.php
@@ -0,0 +1,11 @@
+<?php
+  $txt = "Hola mundo";
+  $x = 5;
+  $y = 10.5;
+
+  /*
+    - Una variable comienza con el $ signo, seguido del nombre de la variable.
+    - Un nombre de variable debe comenzar con una letra o el carácter subrayado.
+    - Un nombre de variable no puede comenzar con un número.
+  */
+?>
